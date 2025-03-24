da69d8b0e0fb3fd8ac4598f847c1eb61c9485301
Yash <ypt7895@gmail.com>
2025-03-25 02:50:17 +0530

    login

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,39 @@
+import { useState, useEffect, useRef } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import axios from 'axios'
+
 const HomePage = () => {
-    return (<div className="middle"><h1>COMING SOON</h1></div>)
+    const [queryParameters] = useSearchParams()
+    const [loggedUser, setLoggedUser] = useState(null)
+    const [redirect, setRedirect] = useState(null)
+    
+    useEffect(()=>{
+        if(loggedUser==null){
+            axios.get('http://localhost:5000/auth/google/authorize')
+            .then(res=>setRedirect(res.data.authorization_url))
+        }
+    },[loggedUser])
+
+    useEffect(()=>{
+        let state = queryParameters.get("state")
+        let code = queryParameters.get("code")
+        let scope = queryParameters.get("scope")
+        if(code){
+            axios.get('http://localhost:5000/auth/google/callback',{
+                params:{state:state,code:code,scope:scope}
+            })
+            .then(res=>{
+                console.log(res)
+                setLoggedUser(res.data)
+            })
+        }
+    },[queryParameters])
+
+    return (
+        <>
+        {loggedUser?<h1>LOGGED IN</h1>:<div className='middle'><h1>COMING SOON</h1><a href={redirect}>LOGIN WITH GOOGLE</a></div>}
+        </>
+    )
 }
 
 export default HomePage
