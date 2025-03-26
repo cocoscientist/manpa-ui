df75867dfcafb84786ed10ae8cc70e124119f523
Yash <ypt7895@gmail.com>
2025-03-26 23:12:54 +0530

    added redirect component

---
+++ src/components/Redirect.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
+const Redirect = () => {
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        const query = window.location.search
+        axios.get(`http://localhost:5000/auth/google/callback${query}`)
+            .then(res=>{
+                console.log(res)
+                localStorage.setItem("token",res.data.access_token)
+                navigate('/app')
+            })
+            .catch(err=>console.log(err))
+    },[navigate])
+
+    return (
+        <>
+        <div className='middle'><h1>REDIRECTING</h1></div>
+        </>
+    )
+}
+
+export default Redirect
