dc5019419b5b0686bd07460636eb98f22994b352
butter-chicken27 <agraj.srivastava60@gmail.com>
2025-04-01 20:13:22 +0530

    fixing import

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import axios from 'axios'
 
 const HomePage = () => {
