e8222668331ad0c02566a22933166f3db3d2b34c
Yash <ypt7895@gmail.com>
2025-04-04 01:36:44 +0530

    articles base components

---
+++ src/components/articles/ArticleCard.jsx
@@ -0,0 +1,24 @@
+import { Card, CardActionArea, Skeleton, CardContent, Typography } from "@mui/material"
+
+const ArticleCard = (props) => {
+    return (
+        <Card>
+            <CardActionArea sx={{ display: 'flex' }}>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {props.title}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        {props.intro}
+                    </Typography>
+                    <Typography variant="subtitle2" sx={{ color: 'text.secondary' }}>
+                        Written by {props.author}
+                    </Typography>
+                </CardContent>
+                <Skeleton sx={{width:'280px'}} animation="wave" variant="rectangular" />
+            </CardActionArea>
+        </Card>
+    )
+}
+
+export default ArticleCard
