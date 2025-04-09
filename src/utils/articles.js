e958d364aa2ccd8599623087e69dba03a242d8dc
Vikhyath-vec <vikhyath.kothamasu@gmail.com>
2025-04-09 16:54:29 +0530

    homepage UI

--- src/utils/articles.js
+++ src/utils/articles.js
@@ -3,30 +3,35 @@ export const articlesList = [
         "id":"article001",
         "title":"Sample Article Title",
         "author":"Author Name",
+        "image":"assets/article1.jpeg",
         "credentials":"Credentials of Author",
         "intro":"This text is an introduction to the content of the article, which is currently a sample article. On clicking this article you will get to learn more about the topic by reading the article."
     },{
         "id":"article002",
         "title":"Sample Article Title",
         "author":"Author Name",
+        "image":"assets/article2.jpg",
         "credentials":"Credentials of Author",
         "intro":"This text is an introduction to the content of the article, which is currently a sample article. On clicking this article you will get to learn more about the topic by reading the article."
     },{
         "id":"article003",
-        "title":"Sample Article Title",
+        "title":"Extended Title with a Lot of Very Many Words",
         "author":"Author Name",
+        "image":"assets/article3.jpeg",
         "credentials":"Credentials of Author",
         "intro":"This text is an introduction to the content of the article, which is currently a sample article. On clicking this article you will get to learn more about the topic by reading the article."
     },{
         "id":"article004",
         "title":"Sample Article Title",
         "author":"Author Name",
+        "image":"assets/article4.jpg",
         "credentials":"Credentials of Author",
         "intro":"This text is an introduction to the content of the article, which is currently a sample article. On clicking this article you will get to learn more about the topic by reading the article."
     },{
         "id":"article005",
         "title":"Sample Article Title",
         "author":"Author Name",
+        "image":"assets/article5.jpeg",
         "credentials":"Credentials of Author",
         "intro":"This text is an introduction to the content of the article, which is currently a sample article. On clicking this article you will get to learn more about the topic by reading the article."
     }
