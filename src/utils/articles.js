34a99df39d02839889fc4def591e959796ecb6cd
butter-chicken27 <agraj.srivastava60@gmail.com>
2025-08-13 23:01:42 +0530

    adding article intros

--- src/utils/articles.js
+++ src/utils/articles.js
@@ -7,7 +7,7 @@ export const articlesList = [
         "bigImage":"assets/dealingwithguilt.png",
         "lightImage":true,
         "credentials":"",
-        "intro":"This text is an introduction to the content of the article, which is currently a sample article. On clicking this article you will get to learn more about the topic by reading the article."
+        "intro":"After walking away from my marriage with two sons and my elderly father, I had to rebuild life from scratch. Here are the lessons I learned on navigating the emotional, social, and practical challenges of single parenthood."
     },
     {
         "id":"mpa002",
@@ -15,15 +15,17 @@ export const articlesList = [
         "author":"Richa Gupta",
         "image":"assets/article1.jpeg",
         "credentials":"",
-        "intro":"This text is an introduction to the content of the article, which is currently a sample article. On clicking this article you will get to learn more about the topic by reading the article."
+        "intro":"Instead of only telling children stories of extraordinary people, show them inspiration through your own actions and home environment. This piece explores how everyday habits, values, and behavior shape a child’s mindset far more than distant success stories."
     },
     {
         "id":"mpa003",
         "title":"Financial Wisdom",
         "author":"Deepak",
         "image":"assets/finance.png",
+        "bigImage":"assets/financialwisdom.png",
+        "lightImage":true,
         "credentials":"",
-        "intro":"This text is an introduction to the content of the article, which is currently a sample article. On clicking this article you will get to learn more about the topic by reading the article."
+        "intro":"Financial literacy starts with understanding the balance between saving and investing, then aligning both with your personal goals. This article outlines practical habits, smart investment principles, and planning tools to help your money work for you."
     },
     {
         "id":"article001",
