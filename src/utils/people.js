f650eb40ae1d0d0fdbaf73378a352a9b6672407f
Yash <ypt7895@gmail.com>
2025-08-17 19:35:35 +0530

    start

--- src/utils/people.js
+++ src/utils/people.js
@@ -3,21 +3,25 @@ export const people = [
         name:"John Doe",
         expertise:"Expert1",
         designation:"PhD",
-        about:"We'll use this field to add and update the description of the expert. Expect a length of 2-3 lines maximum. We hope you have a pleasant experience with this expert."
+        about:"We'll use this field to add and update the description of the expert. Expect a length of 2-3 lines maximum. We hope you have a pleasant experience with this expert.",
+        img:"assets/persona.png"
     },{
         name:"Jane Doe",
         expertise:"Expert2",
         designation:"MBA",
-        about:"We'll use this field to add and update the description of the expert. Expect a length of 2-3 lines maximum. We hope you have a pleasant experience with this expert."
+        about:"We'll use this field to add and update the description of the expert. Expect a length of 2-3 lines maximum. We hope you have a pleasant experience with this expert.",
+        img:"assets/personb.png"
     },{
         name:"Jill Doe",
         expertise:"Expert3",
         designation:"MPhil",
-        about:"We'll use this field to add and update the description of the expert. Expect a length of 2-3 lines maximum. We hope you have a pleasant experience with this expert."
+        about:"We'll use this field to add and update the description of the expert. Expect a length of 2-3 lines maximum. We hope you have a pleasant experience with this expert.",
+        img:"assets/personb.png"
     },{
         name:"Jake Doe",
         expertise:"Expert4",
         designation:"BA",
-        about:"We'll use this field to add and update the description of the expert. Expect a length of 2-3 lines maximum. We hope you have a pleasant experience with this expert."
+        about:"We'll use this field to add and update the description of the expert. Expect a length of 2-3 lines maximum. We hope you have a pleasant experience with this expert.",
+        img:"assets/personc.png"
     }
 ]
